971598e75ce5d72bad15bad70ca425b31fff19c9
husky <zsigmond.tatrai@gmail.com>
2025-12-17 07:53:49 +0100

    2 ora

--- main.cpp
+++ main.cpp
@@ -4,26 +4,24 @@ using namespace std;
 
 int main()
 {
-/*
-    cout << "Hello world!" << endl;
-*/
-    int egesz; //4 byte
-    short kisegesz; //2 byte
-    char ch; //1 byte
-    float valos;
-    double nagyvalos;
-    bool logikai; //1 byte
-
-    string szoveg;
-
-    egesz=3;
-    ch='a';
-
-    cout << "A te szamod: ";
-    cin >> egesz;
-    cout << "A te betud: ";
-    cin >> ch;
-    cout << "A te betud es szamod: " << ch << " es " << egesz << endl;
-
+    float szam;
+    cout << "Kerek egy valos szamot: ";
+    cin >> szam;
+    if (szam>0) {
+        cout << "A szam pozitiv." << endl;
+    } else {
+        /*if (szam==0) {
+            cout << "A szam 0." << endl;
+        } else {
+            cout << "A szam negativ." << endl;
+        }
+        */
+        szam==0 ? cout << "A szam nulla." << endl : cout << "A szam negativ." << endl;
+    }
+    int x;
+    cout << "Kerek egy egesz szamot." << endl;
+    cin >> x;
+    x=x%2?11:10;
+    cout << x << endl;
     return 0;
 }
