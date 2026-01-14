53c9f0c24c98be343cadb9861c8966fddfd5027b
husky <zsigmond.tatrai@gmail.com>
2026-01-14 07:45:24 +0100

    4 ora

--- 1_ora/main.cpp
+++ 1_ora/main.cpp
@@ -4,21 +4,23 @@ using namespace std;
 
 int main()
 {
-    /*
-    for(int i=10;i<100;i=i+2) {
-        if(i%3!=0) {
-        cout << i << ", ";
-        }
+    float szam1, szam2;
+    cout << "Kerek ket pozitiv valos szamot kulon sorba beirva:" << endl;
+    do {
+        cin >> szam1;
+    } while (szam1<=0);
+    cout << "Kerem az elsonel nagyobb, masodik pozitiv valos szamot:" << endl;
+    do {
+        cin >> szam2;
+    } while (szam2<=0 || szam1>=szam2);
+
+    cout << szam1 << " es " << szam2 << " atlaga = " << (szam1+szam2)/2 << endl;
+
+    float atlag=(szam1+szam2)/2, diff=(atlag-szam1)/4, koztes=atlag;
+    while (koztes<szam2 && koztes>szam1) {
+        cout << koztes << endl;
+        koztes=koztes+diff;
     }
-    cout << endl;
-    */
-    int db=0;
-    for (int i=101;i<1000;i=i+2) {
-        if(i%7!=0) {
-            cout << i << ", ";
-            db++;
-        }
-    }
-    cout << endl << db << endl;
+    cout << "Ezek voltakaz atlaghoz adott novendekek";
     return 0;
 }
