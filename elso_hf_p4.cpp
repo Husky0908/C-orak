a79a84f23cf9f7d6db015f8c4ca80ba4b81ad173
husky <zsigmond.tatrai@gmail.com>
2026-01-22 17:42:16 +0100

    elso hazi elenorzessel eggyutt

--- elso_hf_p4.cpp
+++ elso_hf_p4.cpp
@@ -2,10 +2,12 @@
 
 int main() {
     int for_end;
-    
-    std::cout << "Kerek egy pozitiv egesz szamot:" << std::endl;
-    std::cin >> for_end;
 
+    do {
+        std::cout << "Kerek egy pozitiv egesz szamot:" << std::endl;
+        std::cin >> for_end;
+    } while (for_end<1);
+    
     for (int i=1; i<=for_end; i=i+2) {
         std::cout << i << " negyzete " << i*i << std::endl;
     }
